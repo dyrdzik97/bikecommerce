1da0358c21fd8e3ab9d6f7f8cc3f47f93b446125
Adam Dyrda <adam@moonite.co>
2023-02-27 16:18:08 +0100

    Initial comit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+
+  rules: {
+    'no-unused-vars': 'off',
+    'no-console': 'warn',
+    quotes: [2, 'single', { avoidEscape: true }],
+    'comma-dangle': 'off',
+    semi: ['error', 'always'],
+  },
+};

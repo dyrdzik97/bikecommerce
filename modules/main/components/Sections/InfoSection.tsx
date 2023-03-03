1f57d8e7bcaf665089da82f2284267a4f633ad8d
Adam Dyrda <adam@moonite.co>
2023-03-03 16:56:42 +0100

    Info section inject

---
+++ modules/main/components/Sections/InfoSection.tsx
@@ -0,0 +1,7 @@
+const InfoSection = (): JSX.Element => {
+  // here will be and section with image and short descr about offer and services
+  // bg image https://demo2.themelexus.com/ridez/wp-content/uploads/2022/01/h1-bannernew.jpg
+  return <></>;
+};
+
+export default InfoSection;

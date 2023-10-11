db48d852556fdded690813f4c6933062c968a066
Adam Dyrda <adam@moonite.co>
2023-10-11 12:28:28 +0200

    Styling orders

--- modules/products/components/OrderTile/OrderTile.tsx
+++ modules/products/components/OrderTile/OrderTile.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
-import Separator from '../../../ui/components/Separator/Separator';
 import { IProductDTO } from '../../dto/productDTO';
 import { getProductHref } from '../../mappers';
 
@@ -13,7 +12,7 @@ interface IOrderTileProps {
 const OrderTile = ({ orderId, paymentStatus, items }: IOrderTileProps) => {
   const { t } = useTranslation('thankyou');
   return (
-    <ul className='bg-gray-100 w-100 text-gray-600 hover:text-gray-700 mt-3 rounded py-2 px-3 shadow-2xl '>
+    <ul className='bg-gray-100 w-100 text-gray-600 hover:text-gray-700 shadow-2xl mt-3 rounded py-2 px-3 '>
       <li className='w-100 flex items-center py-3'>
         <span>
           {t('orderNumber')} <span className='font-bold'>{orderId}</span>
@@ -24,13 +23,12 @@ const OrderTile = ({ orderId, paymentStatus, items }: IOrderTileProps) => {
           </span>
         </span>
       </li>
-      <Separator className='my-0' />
       {items.map((item) => {
         const { promoPrice, price, currency } = item.price;
 
         return (
           <Link href={getProductHref(item.productName, item.id)} passHref>
-            <li className='flex cursor-pointer items-center justify-between border-b p-2 py-3 py-2 hover:shadow'>
+            <li className='hover:shadow flex cursor-pointer items-center justify-between p-2 py-3 py-2 shadow-custom'>
               <span>{item.productName}</span>
               <div className='flex flex-row items-center gap-2'>
                 <span className='text-xs text-darkgray'>
